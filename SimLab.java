3fab7a1de297042addc1287d19f19613fa3ebc70
padillan4 <padillanathan2@gmail.com>
2019-03-14 17:25:01 -0400

    Add files via upload

---
+++ SimLab.java
@@ -0,0 +1,15 @@
+
+public class SimLab {
+
+	public static void main(String[] args) {
+		int    list_rank, list_size, next_event_type, maxatr = 0, maxlist = 0;
+		float  transfer, sim_time, prob_distrib[];
+		
+		
+	}
+	
+	void init(){
+		
+	}
+
+}
