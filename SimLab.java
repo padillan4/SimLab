32b0633aad168ce32a01e7abc53c9704f516f07a
padillan4 <padillanathan2@gmail.com>
2019-03-14 17:46:54 -0400

    Add files via upload

--- SimLab.java
+++ SimLab.java
@@ -1,14 +1,32 @@
 
 public class SimLab {
 
+	static int MAX_LIST = 25;
+	static int MAX_ATTR = 25;
+	static int     list_rank, list_size, next_event_type, maxatr = 0, maxlist = 0;
+	static double  transfer, sim_time, prob_distrib[];
+	
 	public static void main(String[] args) {
-		int    list_rank, list_size, next_event_type, maxatr = 0, maxlist = 0;
-		float  transfer, sim_time, prob_distrib[];
+		
+		init();
 		
 		
 	}
 	
-	void init(){
+	static void init(){
+		
+		int list, listsize;
+		
+		if(maxlist < 1){
+			maxlist = MAX_LIST;
+		}
+		listsize = maxlist + 1;
+		// initialize system attributes
+		sim_time = 0.0;
+		if(maxatr < 4){
+			maxatr = MAX_ATTR;
+		}
+		
 		
 	}
 
